b8dab73f8e243b6f6b4b5e6b731bafb287b5162d
Nachiket Mistry <nmistry@splunk.com>
2015-06-18 03:01:06 -0700

    support time, pagination & thread comparison

--- appserver/static/pstack_summary.js
+++ appserver/static/pstack_summary.js
@@ -15,24 +15,41 @@ require(['jquery','underscore','splunkjs/mvc','util/console','splunkjs/mvc/simpl
             //flameView.$el.parents('.dashboard-panel').hide();
         }
 
-        submittedTokens.on('change:threadid', function(){
-            // When the token changes...
-            if(!submittedTokens.get('threadid')) {
-                // ... hide the panel if the token is not defined
-                detailView.$el.parents('.dashboard-panel').hide();
-                //flameView.$el.parents('.dashboard-panel').hide();
-            } else {
-                // ... show the panel if the token has a value
-                detailView.$el.parents('.dashboard-panel').show();
-                //flameView.$el.parents('.dashboard-panel').show();
-            }
-        });
+        // submittedTokens.on('change:threadid', function(){
+        //     // When the token changes...
+        //     if(!submittedTokens.get('threadid')) {
+        //         // ... hide the panel if the token is not defined
+        //         detailView.$el.parents('.dashboard-panel').hide();
+        //         //flameView.$el.parents('.dashboard-panel').hide();
+        //     } else {
+        //         // ... show the panel if the token has a value
+        //         detailView.$el.parents('.dashboard-panel').show();
+        //         //flameView.$el.parents('.dashboard-panel').show();
+        //     }
+        // });
         masterView.on('click', function(e) {
             e.preventDefault();
-            var newValue = e.data['click.name2'];
+            var newValue = "",
+                clickValue = e.data['click.name2'],
+                currentValue = unsubmittedTokens.get('form.threadid') || submittedTokens.get('form.threadid') || "";
+
+            currentValue = currentValue.split(',');
+            
+            if (_.contains(currentValue, clickValue)) {
+                newValue = _.difference(currentValue,[clickValue]);
+            }else{
+                newValue = _.union(currentValue, [clickValue]);
+            }
+
+            // get rid of empty value
+            newValue = _.difference(newValue, [""]);
 
             // Submit the value for the sourcetype field
-            unsubmittedTokens.set('form.threadid', newValue);
+            if (newValue.length===0) {
+                unsubmittedTokens.set('form.threadid', undefined);
+            }else{
+                unsubmittedTokens.set('form.threadid', newValue.toString());
+            }
             submittedTokens.set(unsubmittedTokens.toJSON());
             urlTokens.saveOnlyWithPrefix('form\\.', unsubmittedTokens.toJSON(), {
                 replaceState: false
